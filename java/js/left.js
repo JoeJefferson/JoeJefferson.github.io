cd12b7f8a3004499610fb476f3c5b73c4f84598d
root <root@centos-512mb-fra1-01.(none)>
2017-07-16 12:25:28 +0000

    repull all

---
+++ java/js/left.js
@@ -0,0 +1,17 @@
+﻿//自执行函数，显示栏目信息
+(function(){
+	var cName = '第一章，第二章，第三章，第四章，第五章，第六章，第七章，DOWNLOAD';
+	var cLink = '';
+	for(var i = 0; i <= 6;i++){
+		cLink += 'right.html?id='+i+",";
+	}
+	cLink +='http://pan.baidu.com/s/1geCoY11';
+	var cate3 = document.getElementById('cate3');
+	var cateName = cName.split('，');
+	var cateLink = cLink.split(',');
+	var str = '';
+	for (var i = 0 ; i < cateName.length ; i++) {
+		 str += "<li id='cate4' ><a href='"+cateLink[i]+"' target='right-frame'>"+cateName[i]+"</a></li>";
+	}
+	cate3.innerHTML = str;
+})();
