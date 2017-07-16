af8e53773a0bddcbfc389b34cf11aaf9b5ce2bc7
root <root@centos-512mb-fra1-01.(none)>
2017-07-16 12:54:43 +0000

    fix links

--- java/js/left.js
+++ java/js/left.js
@@ -1,11 +1,10 @@
 ﻿//自执行函数，显示栏目信息
 (function(){
-	var cName = '第一章，第二章，第三章，第四章，第五章，第六章，第七章，DOWNLOAD';
+	var cName = '第一章，第二章，第三章，第四章，第五章，第六章，第七章';
 	var cLink = '';
 	for(var i = 0; i <= 6;i++){
 		cLink += 'right.html?id='+i+",";
 	}
-	cLink +='http://pan.baidu.com/s/1geCoY11';
 	var cate3 = document.getElementById('cate3');
 	var cateName = cName.split('，');
 	var cateLink = cLink.split(',');
